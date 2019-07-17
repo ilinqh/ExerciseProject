1229d88a9578a439ae8658efe01cb0fffc69f776
linqinhong <649995267@qq.com>
2019-07-17 15:33:01 +0800

    圆形头像

---
+++ app/src/main/java/lqh/hchomework/h09/view/AvatarView.kt
@@ -0,0 +1,72 @@
+package lqh.hchomework.h09.view
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import lqh.hchomework.R
+import lqh.hchomework.h09.Utils
+
+/**
+ * 功能：圆形头像
+ * -------------------------------------------------------------------------------------------------
+ * 创建者：@author lqh
+ * -------------------------------------------------------------------------------------------------
+ * 创建日期：2019/7/17
+ * -------------------------------------------------------------------------------------------------
+ * 更新历史
+ * 编号|更新日期|更新人|更新内容
+ */
+class AvatarView(context: Context, attr: AttributeSet) : View(context, attr) {
+
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    private val RADIUS = Utils.dp2px(120f)
+    private val BORDER_WIDTH = Utils.dp2px(4f)
+
+    private var avatar: Bitmap
+    private val xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+
+    private val cut = RectF()
+    private val border = RectF()
+
+    init {
+        avatar = getAvatar((RADIUS * 2).toInt())
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        cut.set(width / 2 - RADIUS, height / 2 - RADIUS, width / 2 + RADIUS, height / 2 + RADIUS)
+        border.set(
+            width / 2 - RADIUS - BORDER_WIDTH,
+            height / 2 - RADIUS - BORDER_WIDTH,
+            width / 2 + RADIUS + BORDER_WIDTH,
+            height / 2 + RADIUS + BORDER_WIDTH
+        )
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        paint.color = Color.parseColor("#FDD835")
+        canvas.drawOval(border, paint)
+        val saved = canvas.saveLayer(cut, paint)
+
+        canvas.drawOval(cut, paint)
+        paint.xfermode = xfermode
+        canvas.drawBitmap(avatar, width / 2 - RADIUS, height / 2 - RADIUS, paint)
+        paint.xfermode = null
+
+        canvas.restoreToCount(saved)
+    }
+
+    private fun getAvatar(width: Int): Bitmap {
+        val options = BitmapFactory.Options()
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeResource(resources, R.drawable.avatar_savage, options)
+        options.inJustDecodeBounds = false
+        options.inDensity = options.outWidth
+        options.inTargetDensity = width
+        return BitmapFactory.decodeResource(resources, R.drawable.avatar_savage, options)
+    }
+
+}
