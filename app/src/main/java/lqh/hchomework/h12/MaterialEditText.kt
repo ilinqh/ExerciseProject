bcaba1c43be2504e1fd5655c72ec91550a7d7d30
lqh <649995267@qq.com>
2019-08-20 22:29:26 +0800

    完成 MaterialEditText

--- app/src/main/java/lqh/hchomework/h12/MaterialEditText.kt
+++ app/src/main/java/lqh/hchomework/h12/MaterialEditText.kt
@@ -1,11 +1,19 @@
 package lqh.hchomework.h12
 
+import android.animation.ObjectAnimator
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.AttributeSet
-import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
+import lqh.hchomework.Utils
 
 /**
- * 功能：
+ * 功能：MaterialEditText
  * -------------------------------------------------------------------------------------------------
  * 创建者：@author lqh
  * -------------------------------------------------------------------------------------------------
@@ -16,10 +24,84 @@ import android.widget.EditText
  */
 class MaterialEditText @JvmOverloads constructor(
     context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
-) : EditText(context, attrs, defStyleAttr) {
+    attrs: AttributeSet? = null
+) : AppCompatEditText(context, attrs) {
 
+    companion object {
+        private val TEXT_SIZE = Utils.dp2px(12f)
+        private val TEXT_MARGIN = Utils.dp2px(8f)
 
+        private val VERTICAL_OFFSET = Utils.dp2px(38f)
+        private val VERTICAL_OFFSET_EXTRA = Utils.dp2px(16f)
+    }
 
+    private var floatingLabelShown: Boolean = false
+
+    private var backgroundPadding = Rect()
+
+    var useFloatingLabel: Boolean = true
+        set(value) {
+            field = value
+            background.getPadding(backgroundPadding)
+            if (value) {
+                setPadding(
+                    backgroundPadding.left, (backgroundPadding.top + TEXT_SIZE + TEXT_MARGIN).toInt(),
+                    backgroundPadding.right, backgroundPadding.bottom
+                )
+            } else {
+                setPadding(
+                    backgroundPadding.left, backgroundPadding.top,
+                    backgroundPadding.right, backgroundPadding.bottom
+                )
+            }
+            invalidate()
+        }
+
+    private var floatingLabelFraction = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private val animator by lazy {
+        ObjectAnimator.ofFloat(this@MaterialEditText, "floatingLabelFraction", 0f, 1f)
+    }
+
+    private val paint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG)
+    }
+
+    init {
+        setPadding(paddingStart, (paddingTop + TEXT_SIZE + TEXT_MARGIN).toInt(), paddingEnd, paddingBottom)
+        paint.textSize = TEXT_SIZE
+        addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (floatingLabelShown && TextUtils.isEmpty(s)) {
+                    floatingLabelShown = !floatingLabelShown
+                    animator.reverse()
+                } else if (!floatingLabelShown && !TextUtils.isEmpty(s)) {
+                    floatingLabelShown = !floatingLabelShown
+                    animator.start()
+                }
+            }
+        })
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        if (useFloatingLabel) {
+            paint.alpha = (floatingLabelFraction * 0xff).toInt()
+            canvas.drawText(
+                hint.toString(), paddingStart.toFloat(),
+                VERTICAL_OFFSET - VERTICAL_OFFSET_EXTRA * floatingLabelFraction, paint
+            )
+        }
+    }
 }
---
+++ app/src/main/java/lqh/hchomework/h15/ScalableImageView.kt
@@ -0,0 +1,23 @@
+package lqh.hchomework.h15
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ImageView
+
+/**
+ * 功能：可缩放的 ImageView
+ * -------------------------------------------------------------------------------------------------
+ * 创建者：@author lqh
+ * -------------------------------------------------------------------------------------------------
+ * 创建日期：2019-08-20
+ * -------------------------------------------------------------------------------------------------
+ * 更新历史
+ * 编号|更新日期|更新人|更新内容
+ */
+class ScalableImageView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    def: Int = 0
+): ImageView(context, attrs, def) {
+
+}
