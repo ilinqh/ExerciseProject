454937ff1dc7c0eab284d29be2722f9f6a287dff
lqh <649995267@qq.com>
2019-08-23 19:53:38 +0800

    ScalableImageView 完成

--- app/src/main/java/lqh/hchomework/MainActivity.kt
+++ app/src/main/java/lqh/hchomework/MainActivity.kt
@@ -2,15 +2,11 @@ package lqh.hchomework
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener {
-            editText.useFloatingLabel = !editText.useFloatingLabel
-        }
     }
 }
--- app/src/main/java/lqh/hchomework/h15/ScalableImageView.kt
+++ app/src/main/java/lqh/hchomework/h15/ScalableImageView.kt
@@ -1,8 +1,19 @@
 package lqh.hchomework.h15
 
+import android.animation.ObjectAnimator
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.widget.ImageView
+import android.widget.OverScroller
+import androidx.core.view.GestureDetectorCompat
+import lqh.hchomework.Utils
+import kotlin.math.max
+import kotlin.math.min
 
 /**
  * 功能：可缩放的 ImageView
@@ -18,6 +29,66 @@ class ScalableImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     def: Int = 0
-): ImageView(context, attrs, def) {
+) : ImageView(context, attrs, def), Runnable {
+
+    companion object {
+        private val IMAGE_WIDTH = Utils.dp2px(300f)
+
+        private const val OVER_SCALE_FACTOR = 1.5f
+    }
+
+    private val paint by lazy {
+        Paint(Paint.ANTI_ALIAS_FLAG)
+    }
+
+    private var originalOffsetX: Float = 0f
+    private var originalOffsetY: Float = 0f
+
+    private var offsetX: Float = 0f
+    private var offsetY: Float = 0f
+
+    private var isBig: Boolean = false
+
+    private var smallScale = 1f
+    private var bigScale = 1f
+
+    private val bitmap: Bitmap
+
+    private val detector: GestureDetectorCompat
+
+    private var scaleFraction = 0f
+        set(value) {
+            field = value
+            invalidate()
+        }
+
+    private val animator by lazy {
+        ObjectAnimator.ofFloat(this, "scaleFraction", 0f, 1f)
+    }
+
+    private val scroller: OverScroller
+
+    init {
+        bitmap = Utils.getAvatar(resources, IMAGE_WIDTH.toInt())
+
+        scroller = OverScroller(context)
+        detector =
+            GestureDetectorCompat(context, object : GestureDetector.SimpleOnGestureListener() {
+                override fun onDown(e: MotionEvent?): Boolean {
+                    return true
+                }
+
+                override fun onDoubleTap(e: MotionEvent): Boolean {
+                    isBig = !isBig
+                    if (isBig) {
+                        offsetX = (e.x - width / 2f) * (1 - bigScale / smallScale)
+                        offsetY = (e.y - height / 2f) * (1 - bigScale / smallScale)
+                        fixOffsets()
+                        animator.start()
+                    } else {
+                        animator.reverse()
+                    }
+                    return super.onDoubleTap(e)
+                }
 
 }
